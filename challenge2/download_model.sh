05dc27c8749d7133ebee8db32078951e19bc7d77
fahminlb33 <fahminlb33@gmail.com>
2023-02-19 14:43:01 +0700

    feat: allows script to be executed from different directory

--- challenge2/download_model.sh
+++ challenge2/download_model.sh
@@ -1,5 +1,8 @@
-#/bin/bash
+#!/usr/bin/env bash
+
+# Get absolute path to script directory
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
 
 # Download the model
-wget -O models/best.onnx https://blob.kodesiana.com/kodesiana-ai-public/models/intel_oneapi_hackathon_2022/best.onnx
-wget -O models/1/best.bin https://blob.kodesiana.com/kodesiana-ai-public/models/intel_oneapi_hackathon_2022/best.bin
+wget -O $SCRIPT_DIR/models/best.onnx https://blob.kodesiana.com/kodesiana-ai-public/models/intel_oneapi_hackathon_2022/best.onnx
+wget -O $SCRIPT_DIR/models/1/best.bin https://blob.kodesiana.com/kodesiana-ai-public/models/intel_oneapi_hackathon_2022/best.bin
