2fb0c9fe71f6cec0ab3f9b71a64a8d37561ab60b
fahminlb33 <fahminlb33@gmail.com>
2023-02-19 14:39:35 +0700

    feat: add script to download final model

---
+++ challenge2/download_model.sh
@@ -0,0 +1,5 @@
+#/bin/bash
+
+# Download the model
+wget -O models/best.onnx https://blob.kodesiana.com/kodesiana-ai-public/models/intel_oneapi_hackathon_2022/best.onnx
+wget -O models/1/best.bin https://blob.kodesiana.com/kodesiana-ai-public/models/intel_oneapi_hackathon_2022/best.bin
